dcd92b28cd40720755f013236605a11fb9741359
sharkwebdev <mark172@me.com>
2015-08-01 22:06:00 +0000

    More admin modifications

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -3,9 +3,9 @@ RailsAdmin.config do |config|
   ### Popular gems integration
   ## == Devise ==
    config.authenticate_with do
-     warden.authenticate! scope: :user
+     redirect_to main_app.root_path unless current_user.is_admin?
    end
-   config.current_user_method(&:current_user)
+  # config.current_user_method(&:current_user)
 
   ## == Cancan ==
   # config.authorize_with :cancan
