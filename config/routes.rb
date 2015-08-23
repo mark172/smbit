eb27bc9f1d568af802c5db739e803d2e92ce396a
sharkwebdev <mark172@me.com>
2015-08-23 05:13:32 +0000

    added services and removed turbolinks

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   
   root "pages#index"
   get "/about" => "pages#about"
+  get "/services" => "pages#services"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
